605a77000172b9415fb29e26e7d64886e113dfeb
foogaro <l.fugaro@gmail.com>
2024-02-24 19:02:58 +0100

    Added a SessionConfig class to manage Redis integration specific characteristics.

---
+++ src/main/java/com/foogaro/config/SessionConfig.java
@@ -0,0 +1,34 @@
+package com.foogaro.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.security.jackson2.SecurityJackson2Modules;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisIndexedHttpSession;
+
+@Configuration
+@EnableRedisIndexedHttpSession
+public class SessionConfig implements BeanClassLoaderAware {
+
+    private ClassLoader loader;
+
+    @Bean
+    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+        return new Jackson2JsonRedisSerializer(objectMapper(), Object.class);
+    }
+
+    private ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModules(SecurityJackson2Modules.getModules(this.loader));
+        return mapper;
+    }
+
+    @Override
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        this.loader = classLoader;
+    }
+
+}
